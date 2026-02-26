b6504f424284fc17d7ccd70c633c281cc37e90fa
Ogbewe Osawaru <osawaru.ogbewe@stu.cu.edu.ng>
2026-02-26 16:28:42 +0100

    Added AI gameplay functionality  ,user can choose who to play

--- ai.py
+++ ai.py
@@ -1,5 +1,7 @@
 import boardcode
 import game
+import random
+
 def get_empty_cells(board):
     empty = []
     for i in range(3):
@@ -7,3 +9,49 @@ def get_empty_cells(board):
             if board[i][j] == '-':
                 empty.append((i, j))
     return empty
+
+def ai_turn(board,move):
+    if(move=='X'):
+        opponent='O'
+    else:
+        opponent='X'
+    corners = [(0,0), (0,2), (2,0), (2,2)]
+    available=get_empty_cells(board)
+    #Check for winning moves
+    for combo in available:
+        board[combo[0]][combo[1]]=move
+        if(boardcode.new_winning(board)== move):
+            return combo
+        else:
+            board[combo[0]][combo[1]] = '-'
+    #Blcoking Opponent
+    for combo in available:
+        board[combo[0]][combo[1]]=opponent
+        if(boardcode.new_winning(board)==opponent):
+            board[combo[0]][combo[1]] = '-'
+            return combo
+        else:
+            board[combo[0]][combo[1]] = '-'
+    corner=[(x,y) for (x,y) in corners if (x,y) in available]
+    if corner:
+        return random.choice(corner)
+    #Pick a center
+    if (1,1) in available:
+            return(1,1)
+    return random.choice(available)
+
+
+    
+
+
+'''
+#print(get_empty_cells(board))
+print(ai_turn(board,'o'))
+boardcode.print_board(board)
+print("John doe")
+board=[ 
+       ['x','o','o'],
+       ['o','-','o'],
+       ['o','o','o']
+]
+'''
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import boardcode
 import ai
+import time
 def is_board_full(board):
     a=[row for columns in board for row in columns]
     return '-' not in a
@@ -19,32 +20,107 @@ def player_turn(player,board):
         if boardcode.player_move(player, x, y, board):
             boardcode.print_board(board)
             break
-
+def ai_turn(board, aiplayer):
+    move = ai.ai_turn(board, aiplayer)
+    boardcode.player_move(aiplayer, move[0], move[1], board)
+    boardcode.print_board(board)
+    return boardcode.new_winning(board)
+def check_winner(board):
+    winner = boardcode.new_winning(board)
+    if winner:
+        print(f"{winner} wins")
+        return True
+    if is_board_full(board):
+        print("It's a tie")
+        return True
+    return False       
 def main():
-    #freshboard=boardcode.board.copy() Shallow Copy
-    freshboard = [row[:] for row in boardcode.board]
-    boardcode.print_board(freshboard)
-    player1='X'
-    player2='O'
-    while True:
-        #print(f"free coordinates {ai.get_empty_cells(freshboard)}")# get empty cells
-        player_turn(player1,freshboard)
-        winner=boardcode.new_winning(freshboard)
-        if winner:
-            print(f"{winner} wins")
-            break
-        if is_board_full(freshboard) :
-            print("It's a tie")
-            break
+    mode=input("Would you like to play a friend or AI: ").lower()
+    while mode not in ['ai', 'p']:
+        mode = input("Play against friend (p) or AI (ai)? ").lower()
+    if (mode=='ai'):
+        heads=input("Would you like to go first? y/n")
+        if (heads=='n'):
+            freshboard = [row[:] for row in boardcode.board]
+            #boardcode.print_board(freshboard)
+            player='X'
+            aiplayer='O'
+            winner=boardcode.new_winning(freshboard)
+            while winner==None:
+                #ai_turn(freshboard,aiplayer)
+                print("AI TURN")
+                time.sleep(0.2)
+                winner = ai_turn(freshboard, aiplayer)
+                if winner:
+                    print(f"{winner} wins")
+                    break
+                if is_board_full(freshboard):
+                    print("It's a tie")
+                    break
 
-        player_turn(player2,freshboard) 
-        winner=boardcode.new_winning(freshboard)
-        if winner:
-            print(f"{winner} wins")
-            break
-        if is_board_full(freshboard) :
-            print("It's a tie")
-            break
+                # Players Turn
+                print("Player Turn")
+                player_turn(player,freshboard)
+                winner=boardcode.new_winning(freshboard)
+                if winner:
+                    print(f"{winner} wins")
+                    break
+                if is_board_full(freshboard) :
+                    print("It's a tie")
+                    break
+        if(heads=='y'):
+            freshboard = [row[:] for row in boardcode.board]
+            #boardcode.print_board(freshboard)
+            player='X'
+            aiplayer='O'
+            winner=boardcode.new_winning(freshboard)
+            while winner==None:
+                # Players Turn
+                print("Player Turn")
+                player_turn(player,freshboard)
+                winner=boardcode.new_winning(freshboard)
+                if winner:
+                    print(f"You :{winner}  wins")
+                    break
+                if is_board_full(freshboard) :
+                    print("It's a tie")
+                    break
+                #ai_turn(freshboard,aiplayer)
+                print("AI TURN")
+                time.sleep(0.2)
+                winner = ai_turn(freshboard, aiplayer)
+                if winner:
+                    print(f"{winner} wins")
+                    break
+                if is_board_full(freshboard):
+                    print("It's a tie")
+                    break
+    if(mode=='p'):
+        #freshboard=boardcode.board.copy() Shallow Copy
+        freshboard = [row[:] for row in boardcode.board]
+        boardcode.print_board(freshboard)
+        player1='X'
+        player2='O'
+        while True:
+            #print(f"free coordinates {ai.get_empty_cells(freshboard)}")# get empty cells
+            player_turn(player1,freshboard)
+            winner=boardcode.new_winning(freshboard)
+            if winner:
+                print(f"{winner} wins")
+                break
+            if is_board_full(freshboard) :
+                print("It's a tie")
+                break
+
+            player_turn(player2,freshboard) 
+            winner=boardcode.new_winning(freshboard)
+            if winner:
+                print(f"{winner} wins")
+                break
+            if is_board_full(freshboard) :
+                print("It's a tie")
+                break
+        
 if __name__ == "__main__":
     main() 
     
